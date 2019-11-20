c1585ae79a7809be6920f65e3f0b90e74d5f2ec4
Daniel Cregg <daniel.cregg@gmit.ie>
2019-11-20 16:55:40 +0000

    add code runner config

--- buildRun.sh
+++ buildRun.sh
@@ -1,2 +1,4 @@
 javac -d /tmp/ src/ie/gmit/studentmanager/*.java && gp preview $(gp url 6080) && java -cp /tmp/ ie.gmit.studentmanager.Main
-#For Code runner #java": "javac -d /tmp $dir*.java && killall -9 java && java -cp /tmp $(sed -n 's/package //p' $fullFileName | sed 's/;$/.$fileNameWithoutExt/') &",
+#For code runner
+# compile code && find and kill old java processes and ignore if none found and error && run java command and find package name also run in background
+#"java": "javac -d /tmp $dir*.java && kill -9 $(ps aux | grep \"java -cp /tmp\" | grep -v grep | awk '{print $2}') || true && java -cp /tmp $(sed -n 's/package //p' $fullFileName | sed 's/;$/.$fileNameWithoutExt/') &",
