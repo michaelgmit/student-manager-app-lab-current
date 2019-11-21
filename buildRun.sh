909b6814fd533c25342dd11728547d82efb58f2e
Daniel Cregg <daniel.cregg@gmit.ie>
2019-11-21 10:23:19 +0000

    updated comand to be quiet and give instructions

--- buildRun.sh
+++ buildRun.sh
@@ -1,4 +1,4 @@
-javac -d /tmp/ src/ie/gmit/studentmanager/*.java && gp preview $(gp url 6080) && java -cp /tmp/ ie.gmit.studentmanager.Main
+javac -d /tmp/ src/ie/gmit/studentmanager/*.java 2>/dev/null && gp preview $(gp url 6080) && echo "APP RUNNING - To STOP click inside this terminal window then hold the Ctrl button and press c" && java -cp /tmp/ ie.gmit.studentmanager.Main 2>/dev/null
 #For code runner
 # compile code && find and kill old java processes and ignore if none found and error && run java command and find package name also run in background
 #"java": "javac -d /tmp $dir*.java && kill -9 $(ps aux | grep \"java -cp /tmp\" | grep -v grep | awk '{print $2}') || true && java -cp /tmp $(sed -n 's/package //p' $fullFileName | sed 's/;$/.$fileNameWithoutExt/') &",
